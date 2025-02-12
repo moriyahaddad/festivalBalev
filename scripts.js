281fbceb8141ee676b4a42d4d307903031c6d781
moriyahaddad <128741228+moriyahaddad@users.noreply.github.com>
2025-02-12 12:26:18 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", function () {
             createOrder: function (data, actions) {
                 return actions.order.create({
                     purchase_units: [{
-                        amount: { value: '50.00' }
+                        amount: { value: '01.00' }
                     }]
                 });
             },
